7193e08819ff01daba18d4ba24bd75c6c58a88ba
HimanshuKumar2971 <144801042+HimanshuKumar2444@users.noreply.github.com>
2025-05-31 00:16:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 function insert_Row() {
     //Write your code here
   
+Difficulty Level:
+
+MEDIUM
+Max Score:
+
+30 Points
+Create Table 1
+You are provided with a table and a button, as shown in the image below.
+
+Table Image
+
+Upon pressing the Insert row button, a new row is added to the top of the table, as illustrated below.
+
+Updated Table Image
+
+Instructions
+You are expected to make changes only in the script.js file, where you have to complete the function insert_Row().
+The id of the table is sampleTable.
+The left cell of each new row should have the value New Cell1, and the right cell should have the value New Cell2.
+Examples:
+Before	After
+Before Image	After Image
+- Table has 2 rows	- Table has 3 rows
+- New row added with cells: New Cell1 & New Cell2
+Testing:
+Test Case - Add a Single Row:
+Action: Click the Insert row button.
+Expected Result: The table should have 3 rows, and a new row should be added with cells: New Cell1 & New Cell2.
+Test Case - Add Multiple Rows:
+Action: Click the Insert row button multiple times.
+Expected Result: The table should have an increasing number of rows, and each new row should have cells: New Cell1 & New Cell2.
   
 }
